9f98e34f060ffba6e45b06ae58911e4a04372fdc
John Barnett <jbarnett18@comcast.net>
2019-12-23 15:15:09 -0700

    First commit.

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
+
+function RouterExample() {
+	return (
+		<div>
+			in RouterExample
+		</div>
+	);
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<RouterExample/>, rootElement);
+
+
+
+
+
