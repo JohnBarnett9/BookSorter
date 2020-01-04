c9ec6a29344759f0e7d17e545bdd2a840dfaf6c9
John Barnett <jbarnett18@comcast.net>
2020-01-03 17:44:53 -0700

    Added Router.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
+import {Redirect} from "react-router-dom";
 
 function RouterExample() {
 	return (
-		<div>
-			in RouterExample
-		</div>
+		<BrowserRouter>
+			<div>
+				<ul>
+					<li>
+						<Link to="/John/React/Books">BookTitle</Link>
+					</li>
+					<li>
+						<Link to="/John/React/Books/BookAuthor">BookAuthor</Link>
+					</li>
+				</ul>
+				<Switch>
+					<Route path="/John/React/Books/BookAuthor" component={BookAuthor} >
+					</Route>
+					<Route exact path="/John/React/Books" component={BookTitle} >
+					</Route>
+					<Route  path="*" component={NotFound} >
+						<Redirect to="localhost/John/React/Books" />
+					</Route>
+				</Switch>
+			</div>
+		</BrowserRouter>
 	);
 }
 
+function BookTitle() {
+	return (
+		<div>
+			Book Title
+		</div>
+	);
+}
+function BookAuthor(){
+	return (
+		<div>
+			Book Author
+		</div>
+	);
+}
+function NotFound(){
+	return (
+		<div>
+			Not found...
+		</div>
+	);
+}
 const rootElement = document.getElementById("root");
 ReactDOM.render(<RouterExample/>, rootElement);
 
