fe585c85355f80bfc2bd56d64c9478bf426f9f9b
John Barnett <jbarnett18@comcast.net>
2020-01-06 13:15:29 -0700

    3 ways of sorting happen on home page.

--- src/index.js
+++ src/index.js
@@ -5,32 +5,6 @@ import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 import {Redirect} from "react-router-dom";
 
 const RouterExample = () => {
-	return (
-		<BrowserRouter>
-			<div>
-				<ul>
-					<li>
-						<Link to="/John/React/Books">BookTitle</Link>
-					</li>
-					<li>
-						<Link to="/John/React/Books/BookAuthor">BookAuthor</Link>
-					</li>
-				</ul>
-				<Switch>
-					<Route exact path="/John/React/Books" component={BookTitle} >
-					</Route>
-					<Route path="/John/React/Books/BookAuthor" component={BookAuthor} >
-					</Route>
-					<Route  path="*" component={NotFound} >
-						<Redirect to="localhost/John/React/Books" />
-					</Route>
-				</Switch>
-			</div>
-		</BrowserRouter>
-	);
-};
-
-function BookTitle() {
 	const bookInfo =
 		[
 			{
@@ -46,14 +20,97 @@ function BookTitle() {
 		];
 
 	return (
+		<BrowserRouter>
+			<div>
+				<ul>
+					<li>
+						<Link to="/John/React/Books">BookTitle</Link>
+					</li>
+					<li>
+						<Link to="/John/React/Books/BookAuthor">BookAuthor</Link>
+					</li>
+				</ul>
+				<Switch>
+					<Route
+					exact path="/John/React/Books"
+					component={BookTitle}>
+					</Route>
+					<Route path="/John/React/Books/BookAuthor" component={BookAuthor} >
+					</Route>
+					<Route  path="*" component={NotFound} >
+						<Redirect to="localhost/John/React/Books" />
+					</Route>
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
+};
+
+function BookTitle() {
+	/**/
+	const bookInfo =
+		[
+			{
+			"title": "CThe Great Gatsby",
+			"author": "Fitzgerald",
+			"genre": "XAmerican Fiction"
+			},
+			{
+			"title": "AThe Grapes of Wrath",
+			"author": "Steinbeck",
+			"genre": "YAmerican Fiction"
+			},
+			{
+			"title": "B",
+			"author": "DFitzgerald",
+			"genre": "ZAmerican Fiction"
+			}
+		];
+
+	return (
 		<div>
+			<h1>by title</h1>
 			<table>
 				<tr>
 					<th>Title</th>
 					<th>Author</th>
 					<th>Genre</th>
 				</tr>
-				{bookInfo.map((name, index) => {
+				{bookInfo.sort(function(a,b){return a.title > b.title}).map((name, index) => {
+					return (
+						<tr>
+							<td>{name.title}</td>
+							<td>{name.author}</td>
+							<td>{name.genre}</td>
+						</tr>
+					);
+				})}
+			</table>
+			<h1>by author</h1>
+			<table>
+				<tr>
+					<th>Title</th>
+					<th>Author</th>
+					<th>Genre</th>
+				</tr>
+				{bookInfo.sort(function(a,b){return a.author > b.author}).map((name, index) => {
+					return (
+						<tr>
+							<td>{name.title}</td>
+							<td>{name.author}</td>
+							<td>{name.genre}</td>
+						</tr>
+					);
+				})}
+			</table>
+			<h1>by genre</h1>
+			<table>
+				<tr>
+					<th>Title</th>
+					<th>Author</th>
+					<th>Genre</th>
+				</tr>
+				{bookInfo.sort(function(a,b){return a.genre > b.genre}).map((name, index) => {
 					return (
 						<tr>
 							<td>{name.title}</td>
